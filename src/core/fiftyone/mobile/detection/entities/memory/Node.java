a504ff36b8f15f576c42020ca677010d592af358
Mike <nerru86@gmail.com>
2015-06-26 15:59:46 +0100

    Modified to be extended by the Node implementations for V3.1 and V3.2. Class is now abstract and the rankedSignatureIndexes data member has been removed.

--- src/core/fiftyone/mobile/detection/entities/memory/Node.java
+++ src/core/fiftyone/mobile/detection/entities/memory/Node.java
@@ -1,32 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fiftyone.mobile.detection.entities.memory;
 
 import fiftyone.mobile.detection.Dataset;
-import fiftyone.mobile.detection.entities.BaseEntity;
 import fiftyone.mobile.detection.entities.NodeNumericIndex;
 import fiftyone.mobile.detection.readers.BinaryReader;
 
+/* *********************************************************************
+ * This Source Code Form is copyright of 51Degrees Mobile Experts Limited. 
+ * Copyright 2014 51Degrees Mobile Experts Limited, 5 Charlotte Close,
+ * Caversham, Reading, Berkshire, United Kingdom RG4 7BY
+ * 
+ * This Source Code Form is the subject of the following patent 
+ * applications, owned by 51Degrees Mobile Experts Limited of 5 Charlotte
+ * Close, Caversham, Reading, Berkshire, United Kingdom RG4 7BY: 
+ * European Patent Application No. 13192291.6; and 
+ * United States Patent Application Nos. 14/085,223 and 14/085,301.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0.
+ * 
+ * If a copy of the MPL was not distributed with this file, You can obtain
+ * one at http://mozilla.org/MPL/2.0/.
+ * 
+ * This Source Code Form is "Incompatible With Secondary Licenses", as
+ * defined by the Mozilla Public License, v. 2.0.
+ * ********************************************************************* */
 /**
  * All data is loaded into memory when the entity is constructed.
  */
-public class Node extends fiftyone.mobile.detection.entities.Node {
+public abstract class Node extends fiftyone.mobile.detection.entities.Node {
     /**
      * An array of all the numeric children.
      */
     private NodeNumericIndex[] numericChildren;
-    /**
-     * A list of all the signature indexes that relate to this node.
-     */
-    private int[] rankedSignatureIndexes;
     
     public Node(Dataset dataSet, int offset, BinaryReader reader) {
         super(dataSet, offset, reader);
-        super.numericChildren = readNodeNumericIndexes(dataSet, reader, super.numericChildrenCount);
-        rankedSignatureIndexes = BaseEntity.readIntegerArray(reader, super.signatureCount);
+        super.numericChildren = readNodeNumericIndexes(
+                dataSet, reader, super.numericChildrenCount);
     }
 
     /**
@@ -36,15 +46,5 @@ public class Node extends fiftyone.mobile.detection.entities.Node {
     @Override
     public NodeNumericIndex[] getNumericChildren() {
         return numericChildren;
-    }
-    
-    /**
-     * A list of all the signature indexes that relate to this node.
-     * @return A list of all the signature indexes that relate to this node.
-     */
-    @Override
-    public int[] getRankedSignatureIndexes() {
-        return rankedSignatureIndexes;
-    }
-    
+    } 
 }
