81432ae8b6a1e0a46b030140c0985550b2497b59
Mike <nerru86@gmail.com>
2015-07-03 16:29:41 +0100

    Added licence header.

--- src/core/fiftyone/mobile/detection/entities/IEnumerable.java
+++ src/core/fiftyone/mobile/detection/entities/IEnumerable.java
@@ -3,6 +3,26 @@ package fiftyone.mobile.detection.entities;
 import fiftyone.mobile.detection.readers.BinaryReader;
 import java.util.Iterator;
 
+/* *********************************************************************
+ * This Source Code Form is copyright of 51Degrees Mobile Experts Limited. 
+ * Copyright 2014 51Degrees Mobile Experts Limited, 5 Charlotte Close,
+ * Caversham, Reading, Berkshire, United Kingdom RG4 7BY
+ * 
+ * This Source Code Form is the subject of the following patent 
+ * applications, owned by 51Degrees Mobile Experts Limited of 5 Charlotte
+ * Close, Caversham, Reading, Berkshire, United Kingdom RG4 7BY: 
+ * European Patent Application No. 13192291.6; and 
+ * United States Patent Application Nos. 14/085,223 and 14/085,301.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0.
+ * 
+ * If a copy of the MPL was not distributed with this file, You can obtain
+ * one at http://mozilla.org/MPL/2.0/.
+ * 
+ * This Source Code Form is "Incompatible With Secondary Licenses", as
+ * defined by the Mozilla Public License, v. 2.0.
+ * ********************************************************************* */
 /**
  * Class implements the logic of IEnumerable in C#. When you need to read a 
  * specific number of integer objects from the data file it's best to do so 
