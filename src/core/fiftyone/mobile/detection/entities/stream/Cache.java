cce656685c762bdd37877248f27560dc1b942c6e
Mike <nerru86@gmail.com>
2015-07-06 14:32:57 +0100

    Added method to reset cache. Changed class level javadoc.

--- src/core/fiftyone/mobile/detection/entities/stream/Cache.java
+++ src/core/fiftyone/mobile/detection/entities/stream/Cache.java
@@ -1,5 +1,7 @@
 package fiftyone.mobile.detection.entities.stream;
 
+import fiftyone.mobile.detection.entities.BaseEntity;
+
 /* *********************************************************************
  * This Source Code Form is copyright of 51Degrees Mobile Experts Limited. 
  * Copyright 2014 51Degrees Mobile Experts Limited, 5 Charlotte Close,
@@ -21,25 +23,16 @@ package fiftyone.mobile.detection.entities.stream;
  * defined by the Mozilla Public License, v. 2.0.
  * ********************************************************************* */
 /**
- * Many of the entities used by the detector data set are requested repeatedly.
- * The cache improves memory usage and reduces strain on the garbage collector
- * by storing previously requested entities for a short period of time to avoid
- * the need to re-fetch them from the underlying storage mechanism. <p> The
- * cache works by maintaining two dictionaries of entities keyed on their offset
- * or index. The inactive list contains all items requested since the cache was
- * created or last serviced. The active list contains all the items currently in
- * the cache. The inactive list is always updated when an item is requested. <p>
- * When the cache is serviced the active list is destroyed and the inactive list
- * becomes the active list. i.e. all the items that were requested since the
- * cache was last serviced are now in the cache. A new inactive list is created
- * to store all items being requested since the cache was last serviced.
- * @param <T>
+ * Provides an additional method to reduce the number of parameters passed when 
+ * adding an item to the cache.
+ * 
+ * @param <T> The type of BaseEntity the cache will contain.
  */
-public class Cache<T> extends fiftyone.mobile.detection.Cache {
+public class Cache<T extends BaseEntity> 
+                        extends fiftyone.mobile.detection.Cache<Integer, T> {
 
     /**
      * Constructs a new instance of the cache.
-     *
      * @param cacheSize number of items in cache.
      */
     public Cache(int cacheSize) {
@@ -51,7 +44,18 @@ public class Cache<T> extends fiftyone.mobile.detection.Cache {
      * @param index index of the item.
      * @param item item to add.
      */
-    void addRecent(int index, T item) {
+    public void addRecent(int index, T item) {
         super.addRecent(index, item);
+    }
+    
+    /**
+     * Resets the stats for the cache.
+     */
+    public void resetCache() {
+        super.clearActiveList();
+        super.clearBackgroundList();
+        super.clearMisses();
+        super.clearRequests();
+        super.clearSwitches();
     }
 }
