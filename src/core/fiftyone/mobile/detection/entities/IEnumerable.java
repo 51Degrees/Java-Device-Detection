1c4092032d3bfc35150059d134a18c418fde0d3b
Mike <nerru86@gmail.com>
2015-07-06 17:27:05 +0100

    Updated the class level javadoc and the implementation.

--- src/core/fiftyone/mobile/detection/entities/IEnumerable.java
+++ src/core/fiftyone/mobile/detection/entities/IEnumerable.java
@@ -1,19 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fiftyone.mobile.detection.entities;
 
 import java.util.Iterator;
 
-/**
+/* *********************************************************************
+ * This Source Code Form is copyright of 51Degrees Mobile Experts Limited. 
+ * Copyright 2014 51Degrees Mobile Experts Limited, 5 Charlotte Close,
+ * Caversham, Reading, Berkshire, United Kingdom RG4 7BY
+ * 
+ * This Source Code Form is the subject of the following patent 
+ * applications, owned by 51Degrees Mobile Experts Limited of 5 Charlotte
+ * Close, Caversham, Reading, Berkshire, United Kingdom RG4 7BY: 
+ * European Patent Application No. 13192291.6; and 
+ * United States Patent Application Nos. 14/085,223 and 14/085,301.
  *
- * @author mike
- * @param <T>
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0.
+ * 
+ * If a copy of the MPL was not distributed with this file, You can obtain
+ * one at http://mozilla.org/MPL/2.0/.
+ * 
+ * This Source Code Form is "Incompatible With Secondary Licenses", as
+ * defined by the Mozilla Public License, v. 2.0.
+ * ********************************************************************* */
+/**
+ * Interface to be implemented whenever a C#-style IEnumerable is required. 
+ * Allows the sub class to implement the next and hasNext methods.
+ * 
+ * @param <T> generic type.
  */
-public interface IEnumerable<T extends BaseEntity> extends Iterator<Integer>, Iterable<Integer> {
-    
-    public abstract IEnumerable<T> getRange(int from, int to);
+public interface IEnumerable<T> extends Iterator<T>, Iterable<T> {
     
 }
