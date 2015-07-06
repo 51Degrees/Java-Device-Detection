5872f9a1d60b4321db386cf718ccb20c72456200
Mike <nerru86@gmail.com>
2015-07-06 14:31:11 +0100

    Added licence info and fixed class javadocs.

--- src/core/fiftyone/mobile/detection/entities/stream/FixedCacheList.java
+++ src/core/fiftyone/mobile/detection/entities/stream/FixedCacheList.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fiftyone.mobile.detection.entities.stream;
 
 import fiftyone.mobile.detection.entities.BaseEntity;
@@ -12,12 +7,53 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
+
+/* *********************************************************************
+ * This Source Code Form is copyright of 51Degrees Mobile Experts Limited. 
+ * Copyright 2014 51Degrees Mobile Experts Limited, 5 Charlotte Close,
+ * Caversham, Reading, Berkshire, United Kingdom RG4 7BY
+ * 
+ * This Source Code Form is the subject of the following patent 
+ * applications, owned by 51Degrees Mobile Experts Limited of 5 Charlotte
+ * Close, Caversham, Reading, Berkshire, United Kingdom RG4 7BY: 
+ * European Patent Application No. 13192291.6; and 
+ * United States Patent Application Nos. 14/085,223 and 14/085,301.
  *
- * @author mike
- * @param <T>
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0.
+ * 
+ * If a copy of the MPL was not distributed with this file, You can obtain
+ * one at http://mozilla.org/MPL/2.0/.
+ * 
+ * This Source Code Form is "Incompatible With Secondary Licenses", as
+ * defined by the Mozilla Public License, v. 2.0.
+ * ********************************************************************* */
+/**
+ * A readonly list of fixed length entity types held on persistent storage 
+ * rather than in memory.
+ * 
+ * Entities in the underlying data structure are either fixed length where the 
+ * data that represents them always contains the same number of bytes, or 
+ * variable length where the number of bytes to represent the entity varies.
+ *
+ * This class uses the index of the entity in the accessor. The list is 
+ * typically used by entities that need to be found quickly using a divide and 
+ * conquer algorithm.
+ * 
+ * The constructor will read the header information about the underlying data 
+ * structure. The data for each entity is only loaded when requested via the 
+ * accessor. A cache is used to avoid creating duplicate objects when requested 
+ * multiple times.
+ * 
+ * Data sources which don't support seeking can not be used. Specifically 
+ * compressed data structures can not be used with these lists.
+ * 
+ * Should not be referenced directly.
+ * 
+ * @param <T> The type of BaseEntity the list will contain.
  */
-public class FixedCacheList<T extends BaseEntity> extends StreamFixedList<T> implements ICacheList {
+public class FixedCacheList<T extends BaseEntity> extends StreamFixedList<T> 
+                                                        implements ICacheList {
 
     /**
      * Used to store previously accessed items to improve performance and
