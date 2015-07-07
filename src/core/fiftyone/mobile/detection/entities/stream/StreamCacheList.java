99b67fc699d71acc902b9aa6b018a38e6443f40e
Mike <nerru86@gmail.com>
2015-07-07 10:00:25 +0100

    Initial commit for the StreamCacheList class.

---
+++ src/core/fiftyone/mobile/detection/entities/stream/StreamCacheList.java
@@ -0,0 +1,59 @@
+package fiftyone.mobile.detection.entities.stream;
+
+import fiftyone.mobile.detection.entities.BaseEntity;
+import fiftyone.mobile.detection.factories.BaseEntityFactory;
+import fiftyone.mobile.detection.readers.BinaryReader;
+import java.io.IOException;
+
+/* *********************************************************************
+ * This Source Code Form is copyright of 51Degrees Mobile Experts Limited. 
+ * Copyright 2014 51Degrees Mobile Experts Limited, 5 Charlotte Close,
+ * Caversham, Reading, Berkshire, United Kingdom RG4 7BY
+ * 
+ * This Source Code Form is the subject of the following patent 
+ * applications, owned by 51Degrees Mobile Experts Limited of 5 Charlotte
+ * Close, Caversham, Reading, Berkshire, United Kingdom RG4 7BY: 
+ * European Patent Application No. 13192291.6; and 
+ * United States Patent Application Nos. 14/085,223 and 14/085,301.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0.
+ * 
+ * If a copy of the MPL was not distributed with this file, You can obtain
+ * one at http://mozilla.org/MPL/2.0/.
+ * 
+ * This Source Code Form is "Incompatible With Secondary Licenses", as
+ * defined by the Mozilla Public License, v. 2.0.
+ * ********************************************************************* */
+/**
+ *
+ * @param <T>
+ */
+public class StreamCacheList<T extends BaseEntity> extends BaseList<T> 
+                                                    implements ICacheList {
+
+    public StreamCacheList(Dataset dataSet, BinaryReader reader, BaseEntityFactory<T> entityFactory) {
+        super(dataSet, reader, entityFactory);
+    }
+
+    @Override
+    protected T createEntity(int key, BinaryReader reader) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getPercentageMisses() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public long getSwitches() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void resetCache() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
