41e6de11b584cc41276c9ddffd61e1efc22674b7
Mike <nerru86@gmail.com>
2015-07-07 16:36:30 +0100

    Class is now abstract. Only handles the assignment of the Pool object. Should be implemented in the NodeStreamFactory for v3.1 and v3.2

--- src/core/fiftyone/mobile/detection/factories/stream/NodeStreamFactory.java
+++ src/core/fiftyone/mobile/detection/factories/stream/NodeStreamFactory.java
@@ -1,21 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fiftyone.mobile.detection.factories.stream;
 
 import fiftyone.mobile.detection.entities.stream.Pool;
-import fiftyone.mobile.detection.readers.BinaryReader;
 
+/* *********************************************************************
+ * This Source Code Form is copyright of 51Degrees Mobile Experts Limited. 
+ * Copyright 2014 51Degrees Mobile Experts Limited, 5 Charlotte Close,
+ * Caversham, Reading, Berkshire, United Kingdom RG4 7BY
+ * 
+ * This Source Code Form is the subject of the following patent 
+ * applications, owned by 51Degrees Mobile Experts Limited of 5 Charlotte
+ * Close, Caversham, Reading, Berkshire, United Kingdom RG4 7BY: 
+ * European Patent Application No. 13192291.6; and 
+ * United States Patent Application Nos. 14/085,223 and 14/085,301.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0.
+ * 
+ * If a copy of the MPL was not distributed with this file, You can obtain
+ * one at http://mozilla.org/MPL/2.0/.
+ * 
+ * This Source Code Form is "Incompatible With Secondary Licenses", as
+ * defined by the Mozilla Public License, v. 2.0.
+ * ********************************************************************* */
 /**
  * Factory used to create stream Node entities.
  */
-public class NodeStreamFactory extends fiftyone.mobile.detection.factories.NodeFactory {
+public abstract class NodeStreamFactory 
+                    extends fiftyone.mobile.detection.factories.NodeFactory {
     /**
      * Pool for the corresponding data set used to get readers.
      */
-    private final Pool pool;
+    protected final Pool pool;
     
     /**
      * Constructs a new instance of NodeStreamFactory.
@@ -24,19 +39,4 @@ public class NodeStreamFactory extends fiftyone.mobile.detection.factories.NodeF
     public NodeStreamFactory(Pool pool) {
         this.pool = pool;
     }
-    
-    /**
-     * Constructs a new Node entity from the offset provided.
-     * @param dataSet The data set the node is contained within.
-     * @param index The offset in the data structure to the node.
-     * @param reader Reader connected to the source data structure and 
-     * positioned to start reading.
-     * @return A new Node entity from the data set.
-     */
-    @Override
-    protected fiftyone.mobile.detection.entities.Node construct(fiftyone.mobile.detection.Dataset dataSet, int index, BinaryReader reader) {
-        return new fiftyone.mobile.detection.entities.stream.Node((fiftyone.mobile.detection.entities.stream.Dataset)dataSet, index, reader);
-    }
-
-
 }
