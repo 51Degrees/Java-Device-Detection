18c285b30f41be0c8b395351a44507d2ea199d42
Mike <nerru86@gmail.com>
2015-07-07 11:13:13 +0100

    Implemented Class.

--- src/core/fiftyone/mobile/detection/entities/stream/StreamCacheList.java
+++ src/core/fiftyone/mobile/detection/entities/stream/StreamCacheList.java
@@ -4,6 +4,8 @@ import fiftyone.mobile.detection.entities.BaseEntity;
 import fiftyone.mobile.detection.factories.BaseEntityFactory;
 import fiftyone.mobile.detection.readers.BinaryReader;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /* *********************************************************************
  * This Source Code Form is copyright of 51Degrees Mobile Experts Limited. 
@@ -26,34 +28,91 @@ import java.io.IOException;
  * defined by the Mozilla Public License, v. 2.0.
  * ********************************************************************* */
 /**
- *
- * @param <T>
+ * Lists can be stored as a set of related objects entirely within memory, or 
+ * the relevant objects loaded as required from a file or other permanent store
+ * as required.
+ * 
+ * This class provides core functions needed for lists which load objects
+ * as required. It implements the Cache{T} to store frequently requested
+ * objects and improve memory usage and performance.
+ * 
+ * Delegate methods are used to create new instances of items to add to the 
+ * list in order to avoid creating many inherited list classes for each 
+ * BaseEntity type.
+ * 
+ * Should not be referenced directly.
+ * 
+ * @param <T> The type of BaseEntity the list will contain
  */
-public class StreamCacheList<T extends BaseEntity> extends BaseList<T> 
-                                                    implements ICacheList {
-
-    public StreamCacheList(Dataset dataSet, BinaryReader reader, BaseEntityFactory<T> entityFactory) {
+public abstract class StreamCacheList<T extends BaseEntity> extends BaseList<T> 
+                                                        implements ICacheList {
+    /**
+     * Used to store previously accessed items to improve performance and
+     * reduce memory consumption associated with creating new instances of 
+     * entities already in use.
+     */
+    private final Cache<T> cache;
+    
+    /**
+     * Constructs a new instance of BaseList{T} ready to read entities from 
+     * the source.
+     * @param dataSet Dataset being created.
+     * @param reader Reader used to initialise the header only.
+     * @param entityFactory Used to create new instances of the entity.
+     * @param cacheSize Number of items in list to have capacity to cache.
+     */
+    public StreamCacheList(Dataset dataSet, BinaryReader reader, 
+                            BaseEntityFactory<T> entityFactory, int cacheSize) {
         super(dataSet, reader, entityFactory);
+        this.cache = new Cache<T>(cacheSize);
     }
 
+    /**
+     * Retrieves the entity at the offset or index requested.
+     * @param key Index or offset of the entity required.
+     * @return A new instance of the entity at the offset or index.
+     */
     @Override
-    protected T createEntity(int key, BinaryReader reader) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public T get(int key) {
+        T item = cache.active.get(key);
+        if (item == null) {
+            try {
+                item = super.get(key);
+                cache.active.put(key, item);
+                cache.incrementMissesByOne();
+            } catch (IOException ex) {
+                Logger.getLogger(StreamCacheList.class.getName())
+                                                .log(Level.SEVERE, null, ex);
+            }
+        }
+        cache.addRecent(key, item);
+        cache.incrementRequestsByOne();
+        return item;
     }
 
+    /**
+     * Returns Percentage of request that were not already held in the cache.
+     * @return Percentage of request that were not already held in the cache.
+     */
     @Override
     public double getPercentageMisses() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return (cache != null ? cache.getPercentageMisses() : 0);
     }
 
+    /**
+     * Returns The number of times the cache has been switched.
+     * @return The number of times the cache has been switched.
+     */
     @Override
     public long getSwitches() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return (cache != null ? cache.getCacheSwitches() : 0);
     }
 
+    /**
+     * Resets the cache.
+     */
     @Override
     public void resetCache() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        cache.resetCache();
     }
-    
 }
