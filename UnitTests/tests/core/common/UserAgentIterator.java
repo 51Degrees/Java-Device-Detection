34637ed6f7f3f9d3eb012d4924f7b0faf7d94a48
Mike <nerru86@gmail.com>
2015-07-23 15:12:39 +0100

    Initial commit for java Core tests

---
+++ UnitTests/tests/core/common/UserAgentIterator.java
@@ -0,0 +1,31 @@
+package tests.core.common;
+
+import java.util.Iterator;
+
+/**
+ *
+ */
+public class UserAgentIterator<T> implements Iterator{
+
+    public UserAgentIterator() {
+        
+    }
+    
+    @Override
+    public boolean hasNext() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object next() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+    
+}
