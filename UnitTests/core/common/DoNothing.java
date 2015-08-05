0ae22a3d63325fa11ce32f1fbe8b593d85013df6
James Rosewell <james@51degrees.com>
2015-08-05 18:38:10 +0100

    Fixed no ASCII characters in comments

--- UnitTests/core/common/DoNothing.java
+++ UnitTests/core/common/DoNothing.java
@@ -19,7 +19,7 @@ import fiftyone.mobile.detection.Match;
  * If a copy of the MPL was not distributed with this file, You can obtain
  * one at http://mozilla.org/MPL/2.0/.
  * 
- * This Source Code Form is “Incompatible With Secondary Licenses”, as
+ * This Source Code Form is "Incompatible With Secondary Licenses", as
  * defined by the Mozilla Public License, v. 2.0.
  * ********************************************************************* */
 
