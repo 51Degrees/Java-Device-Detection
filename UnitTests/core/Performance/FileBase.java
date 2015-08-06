7059b9e34ded959ef362efe0b39e6aa36087642a
Mike <nerru86@gmail.com>
2015-08-06 15:34:47 +0100

    Updated as the enumeration class with match methods class moved to a different package.

--- UnitTests/core/Performance/FileBase.java
+++ UnitTests/core/Performance/FileBase.java
@@ -3,7 +3,7 @@ package Performance;
 import common.Asserts;
 import common.Results;
 import common.Utils;
-import fiftyone.mobile.detection.MatchMethods;
+import fiftyone.properties.MatchMethods;
 import fiftyone.mobile.detection.entities.Property;
 import fiftyone.mobile.detection.factories.StreamFactory;
 import java.io.IOException;
