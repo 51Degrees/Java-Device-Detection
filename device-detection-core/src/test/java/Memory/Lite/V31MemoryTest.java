c353952c55301bb1488e04d4fba9934742203a4f
Jo <jo@51degrees.com>
2015-10-09 18:19:57 +0100

    REORG/MAJOR Move all core test files to correct Maven test directory refs #276

---
+++ device-detection-core/src/test/java/Memory/Lite/V31MemoryTest.java
@@ -0,0 +1,65 @@
+package Memory.Lite;
+
+import Memory.MemoryBase;
+import Properties.Constants;
+import common.UserAgentGenerator;
+import java.io.IOException;
+import org.junit.Test;
+
+/* *********************************************************************
+ * This Source Code Form is copyright of 51Degrees Mobile Experts Limited. 
+ * Copyright © 2014 51Degrees Mobile Experts Limited, 5 Charlotte Close,
+ * Caversham, Reading, Berkshire, United Kingdom RG4 7BY
+ * 
+ * This Source Code Form is the subject of the following patent 
+ * applications, owned by 51Degrees Mobile Experts Limited of 5 Charlotte
+ * Close, Caversham, Reading, Berkshire, United Kingdom RG4 7BY: 
+ * European Patent Application No. 13192291.6; and
+ * United States Patent Application Nos. 14/085,223 and 14/085,301.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0.
+ * 
+ * If a copy of the MPL was not distributed with this file, You can obtain
+ * one at http://mozilla.org/MPL/2.0/.
+ * 
+ * This Source Code Form is "Incompatible With Secondary Licenses", as
+ * defined by the Mozilla Public License, v. 2.0.
+ * ********************************************************************* */
+
+public class V31MemoryTest extends MemoryBase {
+
+    public V31MemoryTest() {
+        super(Constants.LITE_PATTERN_V31);
+    }
+
+    @Test
+    public void LiteV31Memory_Memory_UniqueUserAgentsMulti() throws IOException {
+        super.userAgentsMulti(UserAgentGenerator.getUniqueUserAgents(), 200);
+    }
+
+    @Test
+    public void LiteV31Memory_Memory_UniqueUserAgentsSingle() throws IOException {
+        super.userAgentsSingle(UserAgentGenerator.getUniqueUserAgents(), 200);
+    }
+
+    @Test
+    public void LiteV31Memory_Memory_RandomUserAgentsMulti() throws IOException {
+        super.userAgentsMulti(UserAgentGenerator.getRandomUserAgents(), 200);
+    }
+
+    @Test
+    public void LiteV31Memory_Memory_RandomUserAgentsSingle() throws IOException {
+        super.userAgentsSingle(UserAgentGenerator.getRandomUserAgents(), 200);
+    }
+
+    @Test
+    public void LiteV31Memory_Memory_BadUserAgentsMulti() throws IOException {
+        super.userAgentsMulti(UserAgentGenerator.getBadUserAgents(), 200);
+    }
+
+    @Test
+    public void LiteV31Memory_Memory_BadUserAgentsSingle() throws IOException {
+        super.userAgentsSingle(UserAgentGenerator.getBadUserAgents(), 200);
+    }
+}
---
+++ device-detection-core/src/test/java/Performance/PerformanceSuite.java
@@ -0,0 +1,48 @@
+package Performance;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/* *********************************************************************
+ * This Source Code Form is copyright of 51Degrees Mobile Experts Limited. 
+ * Copyright © 2014 51Degrees Mobile Experts Limited, 5 Charlotte Close,
+ * Caversham, Reading, Berkshire, United Kingdom RG4 7BY
+ * 
+ * This Source Code Form is the subject of the following patent 
+ * applications, owned by 51Degrees Mobile Experts Limited of 5 Charlotte
+ * Close, Caversham, Reading, Berkshire, United Kingdom RG4 7BY: 
+ * European Patent Application No. 13192291.6; and
+ * United States Patent Application Nos. 14/085,223 and 14/085,301.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0.
+ * 
+ * If a copy of the MPL was not distributed with this file, You can obtain
+ * one at http://mozilla.org/MPL/2.0/.
+ * 
+ * This Source Code Form is "Incompatible With Secondary Licenses", as
+ * defined by the Mozilla Public License, v. 2.0.
+ * ********************************************************************* */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    Performance.Lite.V31File.class,
+    Performance.Lite.V32File.class,
+    Performance.Lite.V31Array.class,
+    Performance.Lite.V32Array.class,
+    Performance.Lite.V31Memory.class,
+    Performance.Lite.V32Memory.class,
+    Performance.Premium.V31File.class,
+    Performance.Premium.V32File.class,
+    Performance.Premium.V31Array.class,
+    Performance.Premium.V32Array.class,
+    Performance.Premium.V31Memory.class,
+    Performance.Premium.V32Memory.class,
+    Performance.Enterprise.V31File.class,
+    Performance.Enterprise.V32File.class,
+    Performance.Enterprise.V31Array.class,
+    Performance.Enterprise.V32Array.class,
+    Performance.Enterprise.V31Memory.class,
+    Performance.Enterprise.V32Memory.class})
+public class PerformanceSuite {
+}
