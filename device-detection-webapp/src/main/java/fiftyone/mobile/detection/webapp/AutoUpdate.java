5bd058028599d382a80250091c612c08c94cfced
James Rosewell <info@51degrees.com>
2015-10-29 14:57:23 +0000

    OPTIM/MINOR: Fixed AutoUpdate class to work with core.

--- device-detection-webapp/src/main/java/fiftyone/mobile/detection/webapp/AutoUpdate.java
+++ device-detection-webapp/src/main/java/fiftyone/mobile/detection/webapp/AutoUpdate.java
@@ -20,7 +20,6 @@
  * ********************************************************************* */
 package fiftyone.mobile.detection.webapp;
 
-import fiftyone.mobile.detection.AutoUpdateException;
 import fiftyone.mobile.detection.Dataset;
 import fiftyone.mobile.detection.factories.StreamFactory;
 import java.io.File;
@@ -28,14 +27,13 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.TimerTask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class AutoUpdate extends TimerTask {
+public class AutoUpdate extends fiftyone.mobile.detection.AutoUpdate {
 
     private final String masterFilePath;
-    private String[] licenseKeys;
+    private final String[] licenseKeys;
     final private static Logger logger = LoggerFactory
             .getLogger(AutoUpdate.class);
 
@@ -47,34 +45,32 @@ public class AutoUpdate extends TimerTask {
         this.licenseKeys = licenseKeys.toArray(new String[licenseKeys.size()]);
     }
 
-    @Override
     public void run() {
         if (shouldUpdate()) {
             try {
-                boolean success = fiftyone.mobile.detection.AutoUpdate.update(
-                        licenseKeys, masterFilePath);
-                if (success) {
-                    SimpleDateFormat dateFormat = new SimpleDateFormat("y-MMM-d");
-                    final File masterFile = new File(masterFilePath);
-                    Date fileDate = new Date(masterFile.lastModified());
-                    String dateStr = dateFormat.format(fileDate);
-                    logger.info(String.format(
-                            "Automatically updated binary data file '%s' with "
-                            + " version published on the '%s'.",
-                            masterFile,
-                            dateStr));
-                    WebProvider.refresh();
+                switch(fiftyone.mobile.detection.AutoUpdate.update(
+                        licenseKeys, masterFilePath)) {
+                    case AUTO_UPDATE_SUCCESS:
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("y-MMM-d");
+                        final File masterFile = new File(masterFilePath);
+                        Date fileDate = new Date(masterFile.lastModified());
+                        String dateStr = dateFormat.format(fileDate);
+                        logger.info(String.format(
+                                "Automatically updated binary data file '%s' with "
+                                + " version published on the '%s'.",
+                                masterFile,
+                                dateStr));
+                        WebProvider.refresh();
+                        break;
+                    default:
+                        // TODO: Log the warning
+                        break;
                 }
-            } catch (AutoUpdateException ex) {
-                logger.warn(String.format(
-                        "Exception auto updating file '%s'",
-                        masterFilePath),
-                        ex);
             } catch (Exception ex) {
                 logger.warn(String.format(
                         "Exception auto updating file '%s'",
                         masterFilePath),
-                        ex);                
+                        ex);
             }
         }
     }
