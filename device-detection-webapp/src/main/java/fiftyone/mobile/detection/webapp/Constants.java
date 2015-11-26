82657f2110c4f92c6c6fc1e7e322555872f365e4
Mike M <mike@51degrees.com>
2015-11-26 16:13:33 +0000

    REORG/MINOR: updated API version for the Webapp.

--- device-detection-webapp/src/main/java/fiftyone/mobile/detection/webapp/Constants.java
+++ device-detection-webapp/src/main/java/fiftyone/mobile/detection/webapp/Constants.java
@@ -28,7 +28,7 @@ public class Constants {
     /**
      * The version of the API to report in usage sharing.
      */
-    public static final String VERSION = "3.1.8.4";
+    public static final String VERSION = "3.2.2.21";
     
     /**
      * Context parameter in the web.xml file for the file path.
