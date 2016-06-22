a38ace9e8e174f195827112a9f1f0589303dd715
Ben Shillito <ben@51degrees.com>
2016-06-22 16:23:29 +0100

    FEAT: Usage sharing now operates when enabled only when a new session is being created. Enhancements have been made to the background thread processing.
    Request information is only considered for sharing during the first request which is indicated by the isNew property of the session associated with the request.
    
    The background thread and controller are now associated directly with the WebProvider instance and closed when the provider is closed.
    
    Simplified the process of compiling the XML fragments.
    
    Write data in UTF-8 encoding.
    
    Modified methods that don’t need to be public so that they are package local.

--- device-detection-webapp/src/main/java/fiftyone/mobile/detection/webapp/ShareUsage.java
+++ device-detection-webapp/src/main/java/fiftyone/mobile/detection/webapp/ShareUsage.java
@@ -21,11 +21,13 @@
 package fiftyone.mobile.detection.webapp;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.UnknownHostException;
@@ -51,20 +53,16 @@ import org.slf4j.LoggerFactory;
  * You should not access objects of this class directly or instantiate new 
  * objects using this class as they are part of the internal logic.
  */
-class ShareUsage implements Runnable {
+class ShareUsage implements Runnable, Closeable {
 
     /**
-     * Used to signal the thread to check weather to send data.
+     * Used to signal the thread to check whether to send data.
      */
     private final Object wait = new Object();
     /**
-     * Used to hold information from the jar file, default values set below.
-     */
-    private static final String _productName = "51Degrees.mobi - Detection - Java";
-    /**
      * Used to stop the thread.
      */
-    private boolean stop = false;
+    private volatile boolean stop = false;
     /**
      * URL to send new device data to.
      */
@@ -76,7 +74,7 @@ class ShareUsage implements Runnable {
     /**
      * Queue to hold new device detail.
      */
-    private final LinkedBlockingQueue<byte[]> queue;
+    private final LinkedBlockingQueue<String> queue;
     /**
      * The format wanted for dates that are generated.
      */
@@ -85,8 +83,10 @@ class ShareUsage implements Runnable {
     /**
      * Used to detect local devices.
      */
-    private static final String[] local = new String[]{"127.0.0.1",
+    private static final String[] local = new String[]{
+        "127.0.0.1",
         "0:0:0:0:0:0:0:1"};
+   
     /**
      * Creates a logger for this class
      */
@@ -98,104 +98,31 @@ class ShareUsage implements Runnable {
      * @param newDevicesUrl URL of potential new host.
      * @param newDeviceDetail Controls how much data is sent.
      */
-    public ShareUsage(final String newDevicesUrl,
+    ShareUsage(final String newDevicesUrl,
             final NewDeviceDetails newDeviceDetail) {
         // Set TimeZone to UTC
         dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
         this.newDevicesUrl = newDevicesUrl;
         this.newDeviceDetail = newDeviceDetail;
-        this.queue = new LinkedBlockingQueue<byte[]>();
+        this.queue = new LinkedBlockingQueue<String>();
     }
 
     /**
      * Attempts to send any device data that is still yet to be sent and then
      * terminates the Thread.
      */
-    public void destroy() {
-        logger.trace("Destroying 51Degrees.mobi ShareUsage");
+    @Override
+    public void close() {
+        logger.debug("Destroying ShareUsage");
         stop = true;
         synchronized (wait) {
             wait.notifyAll();
         }
-        logger.trace("Destroyed 51Degrees.mobi ShareUsage");
+        logger.debug("Destroyed ShareUsage");
     }
 
     /**
-     * Adds the request details to the queue for processing by the background
-     * thread.
-     *
-     * @param request The current server request.
-     * @throws XMLStreamException
-     * @throws IOException
-     */
-    public void recordNewDevice(final HttpServletRequest request)
-            throws XMLStreamException, IOException {
-        final Enumeration<String> HeaderNames = request.getHeaderNames();
-        final HashMap<String, String> Headers = new HashMap<String, String>();
-
-        while (HeaderNames.hasMoreElements()) {
-            final String n = (String) HeaderNames.nextElement();
-            Headers.put(n, request.getHeader(n));
-        }
-        recordNewDevice(Headers, request.getRemoteAddr(),
-                request.getRequestURI());
-    }
-
-    /**
-     * Adds the request details to the queue for processing by the background
-     * thread.
-     *
-     * @param Headers HashMap containing the request headers.
-     * @param HostAddress the Host Address.
-     * @param URI The URI of the request.
-     * @throws XMLStreamException
-     * @throws IOException
-     */
-    public void recordNewDevice(final HashMap<String, String> Headers,
-            final String HostAddress, String URI) throws XMLStreamException,
-            IOException {
-        // Get the new device details.
-        byte[] data = getContent(Headers, HostAddress, URI, newDeviceDetail);
-
-        if (data != null && data.length > 0) {
-            // Add the new details to the queue for later processing.
-            queue.add(data);
-            synchronized (wait) {
-                // Signal the background thread to check to see if it should
-                // send queued data.
-                wait.notify();
-            }
-        }
-    }
-
-    /**
-     * Sends all the data on the queue.
-     *
-     * @param stream Output stream to send data to.
-     * @throws IOException
-     * @throws XMLStreamException
-     */
-    private void sendData(OutputStream stream) throws IOException,
-            XMLStreamException {
-        GZIPOutputStream compressed = new GZIPOutputStream(stream);
-        PrintWriter pw = new PrintWriter(compressed);
-
-        pw.print("<?xml version=\"1.0\" ?>");
-        pw.print("<Devices>");
-        while (queue.size() > 0) {
-            byte[] item = queue.poll();
-            if (item != null && item.length > 0) {
-                pw.print(new String(item));
-            }
-        }
-        pw.print("</Devices>");
-
-        pw.close();
-        compressed.close();
-    }
-
-    /**
-     * Runs the thread. Used to send the devices data back to 51Degrees.mobi
+     * Runs the thread. Used to send the devices data back to 51Degrees
      * after the NewDeviceQueueLength has been reached.
      */
     @Override
@@ -210,8 +137,7 @@ class ShareUsage implements Runnable {
                 // If there are enough items in the queue, or the thread is
                 // being
                 // stopped send the data.
-                int size = queue.size();
-                if (size >= Constants.NEW_DEVICE_QUEUE_LENGTH
+                if (queue.size() >= Constants.NEW_DEVICE_QUEUE_LENGTH
                         || (stop == true && queue.size() > 0)) {
                     // Prepare the request including all currently queued items.
                     HttpURLConnection request = (HttpURLConnection) new URL(
@@ -220,13 +146,17 @@ class ShareUsage implements Runnable {
                     request.setRequestMethod("POST");
                     request.setUseCaches(false);
                     request.setRequestProperty("Content-Type",
-                            "text/xml; charset=ISO-8859-1");
+                            "text/xml; charset=utf-8");
                     request.setRequestProperty("Content-Encoding", "gzip");
                     request.setDoInput(true);
                     request.setDoOutput(true);
                     try {
                         sendData(request.getOutputStream());
                     } catch (UnknownHostException ex) {
+                        logger.debug(String.format(
+                                "Stopping usage sharing as remote name '%s' " +
+                                "generated UnknownHostException exception.",
+                                newDevicesUrl));
                         stop = true;
                     }
 
@@ -236,19 +166,23 @@ class ShareUsage implements Runnable {
                             new InputStreamReader(request.getInputStream());
                     if (stream.ready()) {
                         BufferedReader response = new BufferedReader(stream);
-                        if (response != null) {
-                            switch (request.getResponseCode()) {
-                                case 200:// OK
-                                    // Ok response, do nothing
-                                    break;
-                                case 408:// Request Timeout
-                                    // Could be temporary, do nothing.
-                                    break;
-                                default:
-                                    // Turn off functionality.
-                                    stop = true;
-                                    break;
-                            }
+                        switch (request.getResponseCode()) {
+                            case 200:// OK
+                                // Ok response, do nothing
+                                break;
+                            case 408:// Request Timeout
+                                // Could be temporary, do nothing.
+                                break;
+                            default:
+                                // Turn off functionality.
+                                logger.debug(String.format(
+                                    "Stopping usage sharing as remote " +
+                                    "name '%s' returned status " +
+                                    "description '%s'.",
+                                    newDevicesUrl, 
+                                    response.readLine()));                                    
+                                stop = true;
+                                break;
                         }
                         // Release the HttpWebResponse
                         response.close();
@@ -273,6 +207,96 @@ class ShareUsage implements Runnable {
                 logger.debug("Illegal State Exception: " + ex);
             }
         } while (stop == false);
+        logger.debug("Share usage stopped.");
+    }
+    
+    /**
+     * Adds the request details to the queue for processing by the background
+     * thread.
+     *
+     * @param request The current server request.
+     * @throws XMLStreamException
+     * @throws IOException
+     */
+    void recordNewDevice(final HttpServletRequest request)
+            throws XMLStreamException, IOException {
+        if (stop == false) {
+            String xml = getContent(request, newDeviceDetail);
+            if (xml != null && xml.length() > 0) {
+                // Add the new details to the queue for later processing.
+                queue.add(xml);
+                synchronized (wait) {
+                    // Signal the background thread to check to see if it should
+                    // send queued data.
+                    wait.notify();
+                }
+            }            
+        }
+    }
+
+    /**
+     * Adds the request details to the queue for processing by the background
+     * thread.
+     *
+     * @param Headers HashMap containing the request headers.
+     * @param HostAddress the Host Address.
+     * @param URI The URI of the request.
+     * @throws XMLStreamException
+     * @throws IOException
+     */
+    void recordNewDevice(final HashMap<String, String> Headers,
+            final String HostAddress, String URI) throws XMLStreamException,
+            IOException {
+
+    }
+
+    /**
+     * Sends all the data on the queue.
+     *
+     * @param stream Output stream to send data to.
+     * @throws IOException
+     * @throws XMLStreamException
+     */
+    private void sendData(OutputStream stream) throws IOException,
+            XMLStreamException {
+        GZIPOutputStream compressed = new GZIPOutputStream(stream);
+        try {
+            PrintWriter pw = new PrintWriter(new OutputStreamWriter(
+                    compressed,
+                    "UTF-8"), true);
+            try {
+                pw.print("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+                pw.print("<Devices>");
+                String fragment = queue.poll();
+                while (fragment != null) {
+                    pw.print(fragment);
+                    fragment = queue.poll();
+                }
+                pw.print("</Devices>");
+            }
+            finally {
+                pw.close();
+            }
+        }
+        finally {
+            compressed.close();
+        }
+    }
+    
+    /**
+     * Returns true if the field provided is one that should not have it's
+     * contents sent to 51degrees for consideration a device matching piece of 
+     * information.
+     * @param field the name of the HTTP header field.
+     * @return True if the field should be passed as blank.
+     */
+    private static Boolean isBlankField(String field) {
+        for (String key : Constants.IGNORE_HEADER_FIELD_VALUES) {
+            if (field.indexOf(key, 0) >= 0) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
@@ -291,30 +315,32 @@ class ShareUsage implements Runnable {
     }
 
     /**
-     * Records the information as XML data and converts to a byte array for
+     * Records the information as XML data and converts to a String for
      * storage.
      *
      * @param Headers A Map of the connection headers.
      * @param HostAddress The address IP address of the host.
      * @param URI The URI information.
      * @param newDeviceDetail How much information to be recorded.
-     * @return The XML data as a byte array.
+     * @return The XML data as a String.
      * @throws XMLStreamException
      * @throws IOException if there was a problem accessing data file.
      */
-    private static byte[] getContent(HashMap<String, String> Headers,
-                                     String HostAddress, 
-                                     String URI, 
+    private static String getContent(final HttpServletRequest request, 
                                      NewDeviceDetails newDeviceDetail)
-                                        throws XMLStreamException, IOException {
+            throws XMLStreamException, IOException {
+        String uri = request.getRequestURI();
+        
         // If the headers contain 51D as a setting or the request is to a web
         // service then do not send the data.
-        boolean ignore = Headers.get("51D") != null || URI.endsWith("asmx");
+        boolean ignore = 
+                request.getHeader("51D") != null || 
+                uri.endsWith("asmx");
 
         if (ignore == false) {
-            XMLOutputFactory factory = XMLOutputFactory.newInstance();
-            ByteArrayOutputStream ms = new ByteArrayOutputStream();
-            XMLStreamWriter writer = factory.createXMLStreamWriter(ms);
+            XMLOutputFactory factory = XMLOutputFactory.newFactory();
+            StringWriter result = new StringWriter();
+            XMLStreamWriter writer = factory.createXMLStreamWriter(result);
 
             // Start writing the device
             writer.writeStartElement("Device");
@@ -324,59 +350,52 @@ class ShareUsage implements Runnable {
             writer.writeEndElement();
 
             writer.writeStartElement("Version");
-            writer.writeCharacters(Constants.VERSION);
+            writer.writeCharacters(
+                    ShareUsage.class.getPackage().getImplementationVersion());
             writer.writeEndElement();
 
             writer.writeStartElement("Product");
-            writer.writeCharacters(_productName);
+            writer.writeCharacters(
+                    ShareUsage.class.getPackage().getImplementationTitle());
             writer.writeEndElement();
 
             // Record either the IP address of the client if not local or the IP
             // address of the machine.
-            // If it contains dots then its an IPV4
-            if (!isLocal(HostAddress)) {
+            final String remoteAddress = request.getRemoteAddr();
+            if (isLocal(remoteAddress) == false) {
                 writer.writeStartElement("ClientIP");
-                writer.writeCharacters(HostAddress);
+                writer.writeCharacters(remoteAddress);
                 writer.writeEndElement();
             }
             
             writer.writeStartElement("ServerIP");
-            writer.writeCharacters(HostAddress);
+            writer.writeCharacters(request.getLocalAddr());
             writer.writeEndElement();
-
-            for (String key : Headers.keySet()) {
-                // Determine if the field should be treated as a blank.
-                boolean blank = false;
-                for (String k : Constants.IGNORE_HEADER_FIELD_VALUES) {
-
-                    blank = key.toLowerCase().contains(k.toLowerCase());
-                    if (blank == true) {
-                        break;
-                    }
-                }
+            
+            final Enumeration<String> headerNames = request.getHeaderNames();
+            while (headerNames.hasMoreElements()) {
                 // Include all header values if maximumDetail is enabled, or
                 // header values related to the useragent or any header
                 // key containing profile or information helpful to determining
                 // mobile devices.
-                if (newDeviceDetail == NewDeviceDetails.MAXIMUM
-                        || key.equals("user-agent") || key.equals("host")
-                        || key.contains("profile") || blank) {
+                final String name = (String)headerNames.nextElement();
+                if (newDeviceDetail == NewDeviceDetails.MAXIMUM ||
+                        name.equals("user-agent") || 
+                        name.equals("host") ||
+                        name.contains("profile")) {
                     // Record the header content if it's not a cookie header.
-                    if (blank) {
-                        writer.writeStartElement("Header");
-                        writer.writeAttribute("Name", key);
-                        writer.writeEndElement();
-                    } else {
-                        writer.writeStartElement("Header");
-                        writer.writeAttribute("Name", key);
-                        writer.writeCData(Headers.get(key));
-                        writer.writeEndElement();
+                    writer.writeStartElement("Header");
+                    writer.writeAttribute("Name", name);
+                    if (isBlankField(name) == false) {
+                        writer.writeCData(request.getHeader(name));
                     }
-                }
-            }
+                    writer.writeEndElement();
+                }                
+            }            
+            
             writer.writeEndElement();
             writer.close();
-            return ms.toByteArray();
+            return result.toString();
         }
         return null;
     }
